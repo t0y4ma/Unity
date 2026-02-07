7bf854734488e281635ba5921b4ce3e6e045deba
Toyama Haruki <th09200611@gmail.com>
2026-02-07 16:34:29 +0900

    2/7時点のコード

---
+++ ClearLine.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ClearLine : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void OnTriggerEnter(Collider other)
+    {
+        Obj obj = other.gameObject.GetComponent<Obj>();
+        if(obj != null) GameManager.instance.objControlManager.ObjectUntouchClearLine(obj.id);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Obj obj= other.gameObject.GetComponent<Obj>();
+        if(obj != null) GameManager.instance.objControlManager.ObjectTouchClearLine(obj.id);
+    }
+}
